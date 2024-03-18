d1270842413cd4b1cc6b8e5920df80856cbdc358
Wilson Castro <wilsoncastro.pv@gmail.com>
2024-03-18 13:48:55 -0300

    Criação da base do projeto e o backend

---
+++ two-nodes/backend/app.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Lista interna para armazenar informações de requisições
+requisicoes = []
+
+@app.route('/armazenar', methods=['POST'])
+def armazenar():
+  """
+  Rota para armazenar informações de requisições.
+
+  Recebe uma requisição JSON e a armazena na lista interna.
+  Retorna a lista completa com a resposta da requisição.
+  """
+  
+  # Obter dados passados na requisição
+  dados_requisicao = request.get_json()
+
+    # Extrair informações da requisição
+  hora_envio = datetime.now()
+  ip_origem = request.remote_addr
+
+  # Criar objeto com informações da requisição
+  dados = {
+    "data": dados_requisicao,
+    "hora_envio": hora_envio,
+    "ip_origem": ip_origem,
+  }
+
+  # Adicionar dados à lista
+  requisicoes.append(dados)
+
+  # Retornar a lista completa
+  return jsonify(requisicoes)
+
+@app.route('/obter', methods=['GET'])
+def obter():
+  """
+  Rota para obter a lista completa de informações de requisições.
+
+  Retorna a lista completa com a resposta da requisição.
+  """
+  # Retornar a lista completa
+  return jsonify(requisicoes)
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
